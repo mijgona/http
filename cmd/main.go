35eb3aaa59e59c99c6448cd2c14bfc3ce1c09c64
Mijgona <mijgon.9191@gmail.com>
2021-05-24 07:05:31 +0500

    Lect23 CRUD-homework

--- cmd/main.go
+++ cmd/main.go
@@ -3,26 +3,32 @@ package main
 import (
 	"log"
 	"net"
+	"net/http"
+	"os"
 
-	"github.com/mijgona/http/pkg/server"
+	"github.com/mijgona/http/cmd/app"
+	"github.com/mijgona/http/pkg/banners"
 )
-
 
 func main() {
 
 	host := "0.0.0.0"
 	port := "9999"
-	srv:=server.NewServer(net.JoinHostPort(host,port))
-
-	srv.AddPath("/","ok", "text/html")
-	srv.AddPath("/about","About Alif Academy", "text/html")
-	srv.AddPath("/api/payment{content}/{id}", "hello", "text/plain")
-
-	err := srv.Start()
-	if err != nil {
-		log.Print(err)
-		return
+	if err := execute(host, port); err != nil {
+		os.Exit(1)
 	}
 }
 
+func execute(host string, port string) (err error) {
+	mux := http.NewServeMux()
+	bannersSvc := banners.NewService()
+	server := app.NewServer(mux, bannersSvc)
+	server.Init()
+	log.Print("start server")
+	srv := &http.Server{
+		Addr:    net.JoinHostPort(host, port),
+		Handler: server,
+	}
 
+	return srv.ListenAndServe()
+}
